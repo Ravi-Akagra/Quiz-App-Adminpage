13848a242bd065d885a5ecb75a18a11e83190ea3
Ravi-Akagra <79597232+Ravi-Akagra@users.noreply.github.com>
2022-11-06 11:07:22 +0530

    Changes

--- js/main.js
+++ js/main.js
@@ -5,35 +5,43 @@
     
     /*==================================================================
     [ Validate ]*/
-    var name = $('.validate-input input[name="name"]');
-    var email = $('.validate-input input[name="email"]');
-    var subject = $('.validate-input input[name="subject"]');
-    var message = $('.validate-input textarea[name="message"]');
+    var question = $('.validate-input textarea[name="question"]');
+    var option1 = $('.validate-input input[name="option1"]');
+    var option2 = $('.validate-input input[name="option2"]');
+    var option3 = $('.validate-input input[name="option3"]');
+    var option4 = $('.validate-input input[name="option4"]');
 
 
     $('.validate-form').on('submit',function(){
         var check = true;
 
-        if($(name).val().trim() == ''){
-            showValidate(name);
+        if($(question).val().trim() == ''){
+            showValidate(question);
             check=false;
         }
 
-        if($(subject).val().trim() == ''){
-            showValidate(subject);
+        if($(option1).val().trim() == ''){
+            showValidate(option1);
+            check=false;
+        }
+        if($(option2).val().trim() == ''){
+            showValidate(option2);
+            check=false;
+        }
+        if($(option3).val().trim() == ''){
+            showValidate(option3);
+            check=false;
+        }
+        if($(option4).val().trim() == ''){
+            showValidate(option4);
             check=false;
         }
 
 
-        if($(email).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-            showValidate(email);
-            check=false;
-        }
-
-        if($(message).val().trim() == ''){
-            showValidate(message);
-            check=false;
-        }
+        // if($(email).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+        //     showValidate(email);
+        //     check=false;
+        // }
 
         return check;
     });
--- js/submit.js
+++ js/submit.js
@@ -1,18 +1,21 @@
 
 var config = {
-    apiKey: "<your-api-key>",
-    authDomain: "<your-auth-domain>",
-    databaseURL: "<your-database-url>",
-    projectId: "<your-project-id>",
-    storageBucket: "<your-storage_bucket>",
-    messagingSenderId: "<your-message-sender-id>"
+    apiKey: "AIzaSyCicfS1soup7DpkyEOlPSbrcOC7CXw-N5I",
+    authDomain: "quizapp-4f0f2.firebaseapp.com",
+    projectId: "quizapp-4f0f2",
+    storageBucket: "quizapp-4f0f2.appspot.com",
+    // databaseURL: "https://quizapp-4f0f2.firebaseio.com",
+    messagingSenderId: "411578106482",
+    appId: "1:411578106482:web:e46b2de7cd8d48744e78eb",
+    measurementId: "G-S9EDS4V0F6"
 };
 firebase.initializeApp(config);
+const db = firebase.firestore();
 window.onload = function () {
     document.getElementById('myform').addEventListener("submit", submitForm);
 }
 
-var db_ref = firebase.database().ref('records');
+
 
 function getInput(id) {
     return document.getElementById(id).value;
@@ -20,15 +23,69 @@ function getInput(id) {
 
 function submitForm(e) {
     e.preventDefault();
-    saveRec(getInput('name_field'), getInput('email_field'), getInput('subject_field'), getInput('message_field'));
+    saveRec(getInput('question_field'),getInput('option1_field'), getInput('option2_field'), getInput('option3_field'), getInput('option4_field'),getInput('topics'));
     document.getElementById("myform").reset();
 }
-function saveRec(name, email, subject, message) {
-    var newRec = db_ref.push();
-    newRec.set({
-        name: name,
-        email: email,
-        subject: subject,
-        message: message
+function saveRec(question,option1, option2, option3, option4,topics) {
+    var lvl;
+    var correctOpt;
+    var difficulty;
+    var iseasy=document.getElementById('Easy').checked;
+    var ismed=document.getElementById('Medium').checked;
+    var isdif=document.getElementById('Difficult').checked;
+    if(iseasy){
+        lvl=document.getElementById('Easy').value;
+    }
+    else if(ismed){
+        lvl=document.getElementById('Medium').value;
+    }
+    else if(isdif){
+        lvl=document.getElementById('Hard').value;
+    }
+
+    if (iseasy){
+        difficulty = "0"
+    }else if(ismed){
+        difficulty = "1"
+    }else if(isdif){
+        difficulty = "2"
+    }
+
+
+    if(document.getElementById('option1').checked){
+        correctOpt = document.getElementById('option1').value;
+    }else if(document.getElementById('option2').checked){
+        correctOpt = document.getElementById('option2').value;
+    }else if(document.getElementById('option3').checked){
+        correctOpt = document.getElementById('option3').value;
+    }else if(document.getElementById('option4').checked){
+        correctOpt = document.getElementById('option4').value;
+    }
+
+    
+    var db_ref = db.collection(lvl)
+    var data = {
+        "question": question,
+        "option1": option1,
+        "option2": option2,
+        "option3": option3,
+        "option4":option4,
+        "isQuestionImage":false,
+        "isOption1Image":false,
+        "isOption2Image":false,
+        "isOption3Image":false,
+        "isOption4Image":false,
+        "difficulty":difficulty,
+        "answer":correctOpt,
+        "topics":topics
+    };
+    console.log(data)
+    db_ref.add(data).then((docRef) => {
+        console.log("Document written with ID: ", docRef.id);
+        alert("Question added to Database");
+    })
+    .catch((error) => {
+        console.error("Error adding document: ", error);
     });
+    
 }
